27e808612855e6e2ead0fb1e6b6b601e12c050b6
federicosimone <federico.simone15@gmail.com>
2026-01-28 20:47:47 +0100

    creazione card con info attrici

--- src/App.jsx
+++ src/App.jsx
@@ -27,12 +27,27 @@ function App() {
         <h1 className="text-center">Attrici e attori famosi</h1>
         <div className="row">
           <div className="col">
-            <ul className="list-unstyled">
-              {attrici.map(attrice => <li>{attrice.name}</li>)}
+            <ul className="list-unstyled flex-wrap d-flex gap-3">
+              {attrici.map(attrice => {
+                return (
+                  <li key={attrice.id}>
+                    <div className="card" style={{ width: "18rem" }}>
+                      <img src="..." class="card-img-top" alt="..."></img>
+                      <div className="card-body">
+                        <h5 className="card-title">{attrice.name}</h5>
+                        <h6 className="card-subtitle mb-2 text-body-secondary">{attrice.birth_year}</h6>
+                        <p>Nazionalità: {attrice.nationality}</p>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                        <a href="#" className="card-link">Card link</a>
+                        <a href="#" className="card-link">Another link</a>
+                      </div>
+                    </div>
+                  </li>)
+              })}
             </ul>
           </div>
         </div>
-      </div>
+      </div >
     </>
   )
 }
